67df06d524f2077e25de09b54f71318709b0820b
hppmm <hppmm@DESKTOP-OH8CFL2>
2023-11-15 21:20:21 +0900

    카테고리 정보를 담을 클래스 생성

---
+++ itnara/src/main/java/kr/ac/kopo/itnara/model/category1.java
@@ -0,0 +1,18 @@
+package kr.ac.kopo.itnara.model;
+
+public class category1 {
+private int category1Code;
+private String name;
+public int getCategory1Code() {
+	return category1Code;
+}
+public void setCategory1Code(int category1Code) {
+	this.category1Code = category1Code;
+}
+public String getName() {
+	return name;
+}
+public void setName(String name) {
+	this.name = name;
+}
+}
---
+++ itnara/src/main/java/kr/ac/kopo/itnara/model/category2.java
@@ -0,0 +1,18 @@
+package kr.ac.kopo.itnara.model;
+
+public class category2 {
+private int category2Code;
+private String name;
+public int getCategory2Code() {
+	return category2Code;
+}
+public void setCategory2Code(int category2Code) {
+	this.category2Code = category2Code;
+}
+public String getName() {
+	return name;
+}
+public void setName(String name) {
+	this.name = name;
+}
+}
